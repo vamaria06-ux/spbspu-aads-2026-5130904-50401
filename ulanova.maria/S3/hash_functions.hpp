9cd813fc4e6624e7d4b185152c40acbb10e53b89
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-15 21:51:58 +0300

    Add HMAC hash wrapper for hash table keys

---
+++ ulanova.maria/S3/hash_functions.hpp
@@ -0,0 +1,67 @@
+#ifndef HASH_FUNCTIONS_HPP
+#define HASH_FUNCTIONS_HPP
+
+#include <cstddef>
+#include <string>
+
+#include <boost/hash2/sha2.hpp>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/get_integral_result.hpp>
+
+namespace ulanova
+{
+  template <class T>
+  struct EqualTo
+  {
+      bool operator()(const T& lhs, const T& rhs) const
+      {
+        return lhs == rhs;
+      }
+  };
+
+  template <class T>
+  class HmacHash
+  {
+    HmacHash():
+      hmac_(defaultKey(), defaultKeySize());
+    {}
+
+    HmacHash(const unsigned char* Key, size_t size):
+      hmac_(key, size)
+    {}
+
+    size_t operator()(cosnt T& value) const
+    {
+      boost::hash2::hmac_sha2_256 hmac(hmac_);
+      boost::hash2::hash_append(hmac, {}, value);
+      return boost::hash2::get_integral_result<size_t>(hmac);
+    }
+  private:
+    boost::hash2::hmac_sha2_256 hmac_;
+
+    static const unsigned char* defaultKey()
+    {
+      static const unsigned char key[] = {
+        0x54, 0x4f, 0x50, 0x49,
+        0x54, 0x5f, 0x53, 0x33,
+        0x5f, 0x48, 0x4d, 0x41,
+        0x43, 0x5f, 0x4b, 0x45,
+        0x59, 0x5f, 0x32, 0x30,
+        0x32, 0x36, 0x5f, 0x41,
+        0x49, 0x50, 0x5f, 0x4c,
+        0x41, 0x42, 0x5f, 0x33
+      };
+
+      return key;
+    }
+
+    static size_t defaultKeySize() noexcept
+    {
+      return 32;
+    }
+  };
+  using StringHash = HmacHash<std::string>;
+  using StringEqual = EqualTo<std::string>;
+}
+
+#endif
