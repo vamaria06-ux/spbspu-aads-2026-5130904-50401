1bdeb7f79789fcac74a8e21238760936028d3a5d
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-21 17:34:38 +0300

    Qualify function definitions with namespace

--- ulanova.maria/S1/functions.cpp
+++ ulanova.maria/S1/functions.cpp
@@ -3,153 +3,151 @@
 #include <cctype>
 #include <limits>
 
-namespace ulanova
+ulanova::List< ulanova::Sequence > ulanova::read_sequences(std::istream& in)
 {
-  List< Sequence > read_sequences(std::istream& in)
+  ulanova::List< ulanova::Sequence > sequences;
+  std::string name;
+
+  while (in >> name)
   {
-    List< Sequence > sequences;
-    std::string name;
+    ulanova::Sequence seq;
+    seq.name = name;
 
-    while (in >> name)
+    while (true)
     {
-      Sequence seq;
-      seq.name = name;
+      int c = in.peek();
 
-      while (true)
+      if (c == EOF || c == '\n')
       {
-        int c = in.peek();
-
-        if (c == EOF || c == '\n')
-        {
-          break;
-        }
-
-        if (std::isdigit(c))
-        {
-          size_t value;
-          in >> value;
-          seq.values.push_back(value);
-        }
-        else
-        {
-          in.get();
-        }
+        break;
       }
 
-      sequences.push_back(seq);
+      if (std::isdigit(c))
+      {
+        size_t value;
+        in >> value;
+        seq.values.push_back(value);
+      }
+      else
+      {
+        in.get();
+      }
     }
 
-    return sequences;
+    sequences.push_back(seq);
   }
 
-  List< List < size_t > > transpose_sequences(const List< Sequence >& seqs)
+  return sequences;
+}
+
+ulanova::List< ulanova::List < size_t > > ulanova::transpose_sequences(const ulanova::List< ulanova::Sequence >& seqs)
+{
+  ulanova::List< ulanova::List < size_t > > result;
+
+  if (seqs.cbegin() == seqs.cend())
   {
-    List< List < size_t > > result;
-
-    if (seqs.cbegin() == seqs.cend())
-    {
-      return result;
-    }
-
-    List< LCIter < size_t > > iters;
-
-    for (LCIter< Sequence > it = seqs.cbegin(); it != seqs.cend(); ++it)
-    {
-      iters.push_back(it->values.cbegin());
-    }
-
-    bool done = false;
-
-    while (!done)
-    {
-      done = true;
-
-      List< size_t >  row;
-
-      LCIter< Sequence > seq_it = seqs.cbegin();
-      LIter< LCIter < size_t > > iter_it = iters.begin();
-
-      for (; seq_it != seqs.cend(); ++seq_it, ++iter_it)
-      {
-        LCIter< size_t > & cur = *iter_it;
-
-        if (cur != seq_it->values.cend())
-        {
-          row.push_back(*cur);
-          ++cur;
-          done = false;
-        }
-      }
-
-      if (row.cbegin() != row.cend())
-      {
-        result.push_back(row);
-      }
-    }
-
     return result;
   }
 
-  List< size_t >  calculate_sums(const List< List < size_t > >& seqs)
+  ulanova::List< ulanova::LCIter < size_t > > iters;
+
+  for (ulanova::LCIter < ulanova::Sequence > it = seqs.cbegin(); it != seqs.cend(); ++it)
   {
-    List< size_t >  sums;
-
-    for (LCIter< List < size_t > > it = seqs.cbegin(); it != seqs.cend(); ++it)
-    {
-      size_t sum = 0;
-
-      for (LCIter< size_t >  jt = it->cbegin(); jt != it->cend(); ++jt)
-      {
-        size_t value = *jt;
-        if (sum > std::numeric_limits< size_t > ::max() - value)
-        {
-          std::cerr << "overflow\n";
-          std::exit(1);
-        }
-        sum += value;
-      }
-
-      sums.push_back(sum);
-    }
-
-    return sums;
+    iters.push_back(it->values.cbegin());
   }
 
-  void print_sequences(std::ostream& out, const List< List < size_t > >& seqs)
+  bool done = false;
+
+  while (!done)
   {
-    for (LCIter< List < size_t > > it = seqs.cbegin(); it != seqs.cend(); ++it)
+    done = true;
+
+    ulanova::List< size_t >  row;
+
+    ulanova::LCIter< ulanova::Sequence > seq_it = seqs.cbegin();
+    ulanova::LIter< ulanova::LCIter < size_t > > iter_it = iters.begin();
+
+    for (; seq_it != seqs.cend(); ++seq_it, ++iter_it)
     {
-      bool first = true;
+      ulanova::LCIter< size_t > & cur = *iter_it;
 
-      for (LCIter< size_t >  jt = it->cbegin(); jt != it->cend(); ++jt)
+      if (cur != seq_it->values.cend())
       {
-        if (!first)
-        {
-          out << " ";
-        }
-
-        out << *jt;
-        first = false;
+        row.push_back(*cur);
+        ++cur;
+        done = false;
       }
+    }
 
-      out << "\n";
+    if (row.cbegin() != row.cend())
+    {
+      result.push_back(row);
     }
   }
-  void print_sums(std::ostream& out, const List< size_t > & sums)
+
+  return result;
+}
+
+ulanova::List< size_t >  ulanova::calculate_sums(const ulanova::List< ulanova::List < size_t > >& seqs)
+{
+  ulanova::List< size_t >  sums;
+
+  for (ulanova::LCIter< ulanova::List < size_t > > it = seqs.cbegin(); it != seqs.cend(); ++it)
+  {
+    size_t sum = 0;
+
+    for (ulanova::LCIter< size_t >  jt = it->cbegin(); jt != it->cend(); ++jt)
+    {
+      size_t value = *jt;
+      if (sum > std::numeric_limits< size_t > ::max() - value)
+      {
+        std::cerr << "overflow\n";
+        std::exit(1);
+      }
+      sum += value;
+    }
+
+    sums.push_back(sum);
+  }
+
+  return sums;
+}
+
+void ulanova::print_sequences(std::ostream& out, const ulanova::List< ulanova::List < size_t > >& seqs)
+{
+  for (ulanova::LCIter< ulanova::List < size_t > > it = seqs.cbegin(); it != seqs.cend(); ++it)
   {
     bool first = true;
 
-    for (LCIter< size_t >  it = sums.cbegin(); it != sums.cend(); ++it)
+    for (ulanova::LCIter< size_t >  jt = it->cbegin(); jt != it->cend(); ++jt)
     {
       if (!first)
       {
         out << " ";
       }
 
-      out << *it;
+      out << *jt;
       first = false;
     }
 
     out << "\n";
   }
+}
+
+void ulanova::print_sums(std::ostream& out, const ulanova::List< size_t > & sums)
+{
+  bool first = true;
+
+  for (ulanova::LCIter< size_t >  it = sums.cbegin(); it != sums.cend(); ++it)
+  {
+    if (!first)
+    {
+      out << " ";
+    }
+
+    out << *it;
+    first = false;
+  }
+
+  out << "\n";
 }
