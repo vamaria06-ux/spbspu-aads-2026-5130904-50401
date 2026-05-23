9f5500afce48dec3bb7b15e3b35fb27de37fdeef
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-23 22:43:37 +0300

    add s4 main command loop

--- ulanova.maria/S4/main.cpp
+++ ulanova.maria/S4/main.cpp
@@ -1,4 +1,83 @@
-#include <iostream>
+#include "commands.hpp"
 
-int main()
-{}
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace
+{
+  void processLine(ulanova::Storage & storage, const std::string & line)
+  {
+    std::istringstream input(line);
+
+    std::string command;
+    if (!(input >> command))
+    {
+      return;
+    }
+
+    if (command == "print")
+    {
+      ulanova::print(std::cout, input, storage);
+    }
+    else if (command == "complement")
+    {
+      ulanova::complement(std::cout, input, storage);
+    }
+    else if (command == "intersect")
+    {
+      ulanova::intersect(std::cout, input, storage);
+    }
+    else if (command == "union")
+    {
+      ulanova::unionDicts(std::cout, input, storage);
+    }
+    else
+    {
+      throw std::runtime_error("invalid command");
+    }
+
+    std::string extra;
+    if (input >> extra)
+    {
+      throw std::runtime_error("invalid command");
+    }
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  if (argc < 2)
+  {
+    std::cerr << "filename is required\n";
+    return 1;
+  }
+
+  ulanova::Storage storage;
+
+  try
+  {
+    ulanova::loadStorage(storage, argv[1]);
+
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+      try
+      {
+        processLine(storage, line);
+      }
+      catch (...)
+      {
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
+  }
+  catch (const std::exception & error)
+  {
+    std::cerr << error.what() << '\n';
+    return 1;
+  }
+
+  return 0;
+}
