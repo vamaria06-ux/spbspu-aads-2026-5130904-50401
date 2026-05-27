80d460903c9c5f5f6401b1df9e4d29d6f22859e6
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-28 00:11:27 +0300

    CG49 in math_functions.cpp

--- ulanova.maria/S2/math_functions.cpp
+++ ulanova.maria/S2/math_functions.cpp
@@ -1,65 +1,64 @@
 #include "math_functions.hpp"
 #include <climits>
+#include <stdexcept>
 
-namespace ulanova
+
+bool ulanova::is_operator(char c)
 {
-  bool is_operator(char c)
+  return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
+}
+int priority(char op)
+{
+  if (op == '+' || op == '-' || op == '^')
   {
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
+    return 1;
   }
-  int priority(char op)
+  if ( op == '*' || op == '/' || op == '%')
   {
-    if (op == '+' || op == '-' || op == '^')
-    {
-      return 1;
-    }
-    if ( op == '*' || op == '/' || op == '%')
-    {
-      return 2;
-    }
-    return 0;
+    return 2;
   }
-  long long apply_op(long long a, long long b, char op)
+  return 0;
+}
+long long ulanova::apply_op(long long a, long long b, char op)
+{
+  switch (op)
   {
-    switch (op)
-    {
-      case '+' :
-        if ((b > 0 && a > LLONG_MAX - b ) || (b < 0 && LLONG_MIN - b))
-        {
-          throw std::overflow_error("overflow");
-        }
-        return a + b;
-      case '-' :
-        if (( b < 0 && a > LLONG_MAX + b) || (b > 0 && a < LLONG_MIN + b))
-        {
-          throw std::overflow_error("underflow");
-        }
-        return a - b;
-      case '*' :
-        if (a != 0 && (b > LLONG_MAX / a || b < LLONG_MIN / a))
-        {
-          throw std::overflow_error("overflow");
-        }
-        return a * b;
-      case '/' :
-        if ( b == 0)
-        {
-          throw std::runtime_error("division by zero");
-        }
-        if (a == LLONG_MIN && b == -1)
-        {
-          throw std::overflow_error("overflow");
-        }
-        return a / b;
-      case '%' :
-        if ( b == 0)
-        {
-          throw std::runtime_error("modul by zero");
-        }
-        return ((a % b) + b) % b;
-      case '^' : return a ^ b;
-      default:
-        throw std::runtime_error("unknown operator");
-    }
+    case '+':
+      if ((b > 0 && a > LLONG_MAX - b ) || (b < 0 && LLONG_MIN - b))
+      {
+        throw std::overflow_error("overflow");
+      }
+      return a + b;
+    case '-':
+      if (( b < 0 && a > LLONG_MAX + b) || (b > 0 && a < LLONG_MIN + b))
+      {
+        throw std::overflow_error("underflow");
+      }
+      return a - b;
+    case '*':
+      if (a != 0 && (b > LLONG_MAX / a || b < LLONG_MIN / a))
+      {
+        throw std::overflow_error("overflow");
+      }
+      return a * b;
+    case '/':
+      if ( b == 0)
+      {
+        throw std::runtime_error("division by zero");
+      }
+      if (a == LLONG_MIN && b == -1)
+      {
+        throw std::overflow_error("overflow");
+      }
+      return a / b;
+    case '%':
+      if ( b == 0)
+      {
+        throw std::runtime_error("modul by zero");
+      }
+      return ((a % b) + b) % b;
+    case '^': return a ^ b;
+    default:
+      throw std::runtime_error("unknown operator");
   }
 }
