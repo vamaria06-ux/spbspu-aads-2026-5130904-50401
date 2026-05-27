6faccc03829e1d1480781aff968f8b258e23e7ea
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-05-28 00:49:45 +0300

    fix unit

--- ulanova.maria/S2/test-infix.cpp
+++ ulanova.maria/S2/test-infix.cpp
@@ -3,5 +3,22 @@
 
 BOOST_AUTO_TEST_CASE(test_priority_infix)
 {
-  BOOST_TEST_CHECK(ulanova::infix_to_postfix("2 + 3 * 4") == "2 3 4 * + ");
+  ulanova::Queue< std::string > result = ulanova::infix_to_postfix("2 + 3 * 4");
+
+  BOOST_TEST_CHECK(result.front() == "2");
+  result.pop();
+
+  BOOST_TEST_CHECK(result.front() == "3");
+  result.pop();
+
+  BOOST_TEST_CHECK(result.front() == "4");
+  result.pop();
+
+  BOOST_TEST_CHECK(result.front() == "*");
+  result.pop();
+
+  BOOST_TEST_CHECK(result.front() == "+");
+  result.pop();
+
+  BOOST_TEST_CHECK(result.empty());
 }
--- ulanova.maria/S2/test-postfix.cpp
+++ ulanova.maria/S2/test-postfix.cpp
@@ -3,15 +3,39 @@
 
 BOOST_AUTO_TEST_CASE(test_simple_postfix)
 {
-  BOOST_TEST_CHECK(ulanova::postfix("2 3 +") == 5);
-  BOOST_TEST_CHECK(ulanova::postfix("4 3 +") == 7);
-  BOOST_TEST_CHECK(ulanova::postfix("10 2 /") == 5);
+  ulanova::Queue< std::string > expr1;
+  expr1.push("2");
+  expr1.push("3");
+  expr1.push("+");
+  BOOST_TEST_CHECK(ulanova::postfix(expr1) == 5);
+
+  ulanova::Queue< std::string > expr2;
+  expr2.push("4");
+  expr2.push("3");
+  expr2.push("+");
+  BOOST_TEST_CHECK(ulanova::postfix(expr2) == 7);
+
+  ulanova::Queue< std::string > expr3;
+  expr3.push("10");
+  expr3.push("2");
+  expr3.push("/");
+  BOOST_TEST_CHECK(ulanova::postfix(expr3) == 5);
 }
 BOOST_AUTO_TEST_CASE(test_complex_postfix)
 {
-  BOOST_TEST_CHECK(ulanova::postfix("2 3 + 4 *") == 20);
+  ulanova::Queue< std::string > expr;
+  expr.push("2");
+  expr.push("3");
+  expr.push("+");
+  expr.push("4");
+  expr.push("*");
+  BOOST_TEST_CHECK(ulanova::postfix(expr) == 20);
 }
 BOOST_AUTO_TEST_CASE(test_xor_postfix)
 {
-  BOOST_TEST_CHECK(ulanova::postfix("5 3 ^") == 6);
+  ulanova::Queue< std::string > expr;
+  expr.push("5");
+  expr.push("3");
+  expr.push("^");
+  BOOST_TEST_CHECK(ulanova::postfix(expr) == 6);
 }
